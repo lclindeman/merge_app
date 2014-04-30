586154b5dc7986eec2d567988fa403805a03bbb1
Jeremy Samples <judesamp@gmail.com>
2014-04-30 10:58:56 -0400

    fixed conflict

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,11 @@
 class UsersController < ApplicationController::Base
 
-<<<<<<< Updated upstream
+
   def jeremy_was_here
     "Hello world"
-=======
-  class jeremy_was_definitely_here
+
+  def jeremy_was_definitely_here
     "Goodbye, cruel world"
->>>>>>> Stashed changes
   end
 
 end
