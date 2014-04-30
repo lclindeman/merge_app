62790598d768642cabdc003f1cb3e64b7a42af15
Jeremy Samples <judesamp@gmail.com>
2014-04-30 10:57:22 -0400

    second change

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController::Base
 
+<<<<<<< Updated upstream
   def jeremy_was_here
     "Hello world"
+=======
+  class jeremy_was_definitely_here
+    "Goodbye, cruel world"
+>>>>>>> Stashed changes
   end
 
 end
